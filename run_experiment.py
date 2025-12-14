39c818e172218512f6408b91421dae1db81947bc
mathiisk <60685836+mathiisk@users.noreply.github.com>
2025-12-14 14:28:25 +0100

    remove commented out function

--- run_experiment.py
+++ run_experiment.py
@@ -7,58 +7,6 @@ from helpers.train import train
 import time
 
 
-# def run_single_experiment(cfg, model_name, dataset_name, sampling_ratio, sampler_name, seed, verbose=True):
-#     set_seed(seed)
-#     dataset, data = load_dataset(dataset_name)
-#     model = cfg.models[model_name](
-#         in_dim=dataset.num_node_features,
-#         out_dim=dataset.num_classes,
-#         params=cfg
-#     )
-
-#     if sampling_ratio == 1.0:
-#         sampled_data = create_data_masks(data, cfg)
-#         sampler_label = "full_graph"
-#     else:
-#         sampler = cfg.samplers[sampler_name](params=cfg)
-#         sampled_data = create_sample(data, sampler, sampling_ratio)
-#         sampled_data = create_data_masks(sampled_data, cfg)
-#         sampler_label = sampler_name
-
-#     num_sampled_nodes = sampled_data.num_nodes
-#     num_sampled_edges = sampled_data.edge_index.size(1)
-
-#     optimizer = cfg.optimizer(
-#         model.parameters(), lr=cfg.lr, weight_decay=cfg.weight_decay
-#     )
-#     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-#         optimizer,
-#         mode="min",
-#         patience=cfg.lr_schedule_patience,
-#         factor=cfg.lr_reduce_factor,
-#         min_lr=cfg.min_lr,
-#     )
-#     final_scores = train(
-#         model,
-#         sampled_data,
-#         optimizer,
-#         scheduler,
-#         cfg,
-#         verbose=verbose
-#     )
-
-#     return {
-#         "dataset": dataset_name,
-#         "model": model_name,
-#         "sampler": sampler_label,
-#         "ratio": sampling_ratio,
-#         "train_f1": final_scores["train"]["f1"],
-#         "val_f1": final_scores["val"]["f1"],
-#         "test_f1": final_scores["test"]["f1"],
-#         "num_nodes": num_sampled_nodes,
-#         "num_edges": num_sampled_edges,
-#     }
-
 def run_single_experiment(cfg, model_name, dataset_name, sampling_ratio, sampler_name, seed, verbose=True):
     set_seed(seed)
     dataset, data = load_dataset(dataset_name)
@@ -153,4 +101,5 @@ def run_experiments(verbose=True):
 
 
 if __name__ == "__main__":
+
     run_experiments()
